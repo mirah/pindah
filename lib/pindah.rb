43e3be183ad6177566572ba7dea71120dc9da3b1
Phil Hagelberg <technomancy@gmail.com>
2011-02-09 22:36:44 -0800

    Hoe doesn't like dev versions.

--- lib/pindah.rb
+++ lib/pindah.rb
@@ -12,7 +12,7 @@ end
 require "mirah"
 
 module Pindah
-  VERSION = '0.5.0-dev'
+  VERSION = '0.5.0'
 
   def self.infer_sdk_location(path)
     tools = path.split(":").detect {|p| File.exists? "#{p}/android" }
